f28ce5f3207eab2eb82072d37f75c114b486b829
yagoramires <yago.ramiresx@gmail.com>
2023-04-22 17:16:02 -0300

    style: ajuste na centralizacao do grid de projetos

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -88,7 +88,7 @@ const Projects = () => {
           as Designer
         </button>
       </div>
-      <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+      <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center'>
         {active.dev &&
           projectsDev.map((project, index) => (
             <ProjectCard
