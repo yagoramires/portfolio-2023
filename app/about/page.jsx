8c552c4cb9d44f79fbe3f13ecc4600d01f66bb2a
yagoramires <yago.ramiresx@gmail.com>
2023-04-25 20:37:06 -0300

    style: ajuste na animacao da home

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,68 +1,16 @@
 import React from 'react';
 import Card from './components/Card';
-import { BsCircleFill } from 'react-icons/bs';
 import { TfiRulerAlt2 } from 'react-icons/tfi';
 import { FaRegEdit } from 'react-icons/fa';
 import Timeline from './components/Timeline';
+import { abilities, education } from '../mock.js';
 
 export const metadata = {
   title: 'About | Portfolio Wladmir',
 };
 
 const About = () => {
-  const abilities = [
-    {
-      title: 'Web Development',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'Mobile Development',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'Marketing',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'Web Designer',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'Graphic Designer',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'UX / UI Designer',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-  ];
-
-  const education = [
-    {
-      title: 'AIT University 2023',
-      label: 'Software Developer',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'FECAP 2007',
-      label: 'Marketing and Advertising with major in Graphic Design',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-    {
-      title: 'SENAC 2010',
-      label: 'Marketing and Advertising with major in Graphic Design',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
-    },
-  ];
+  console.log(abilities);
 
   return (
     <div className=''>
---
+++ app/mock.js
@@ -0,0 +1,53 @@
+export const abilities = [
+  {
+    title: 'Web Development',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'Mobile Development',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'Marketing',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'Web Designer',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'Graphic Designer',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'UX / UI Designer',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+];
+
+export const education = [
+  {
+    title: 'AIT University 2023',
+    label: 'Software Developer',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'FECAP 2007',
+    label: 'Marketing and Advertising with major in Graphic Design',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+  {
+    title: 'SENAC 2010',
+    label: 'Marketing and Advertising with major in Graphic Design',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam eius debitis quia dolorum non magnam',
+  },
+];
