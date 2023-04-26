075fd92cc63879e953c1008fb6ff7baaee3e2210
yagoramires <yago.ramiresx@gmail.com>
2023-04-26 19:20:25 -0300

    remove next fetch test

--- next.config.js
+++ next.config.js
@@ -6,25 +6,25 @@ const nextConfig = {
   images: {
     domains: ['imgur.com', 'i.imgur.com', 'github.com'],
   },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*?)',
-        headers: [
-          {
-            key: 'Referrer-Policy',
-            value: 'no-referrer',
-          },
-          {
-            key: 'Cache-Control',
-            value:
-              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // Apply these headers to all routes in your application.
+  //       source: '/(.*?)',
+  //       headers: [
+  //         {
+  //           key: 'Referrer-Policy',
+  //           value: 'no-referrer',
+  //         },
+  //         {
+  //           key: 'Cache-Control',
+  //           value:
+  //             'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
